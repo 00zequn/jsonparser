24e7ac6ae93140c0a185d187d7be09208530f097
Alex <Ales@123.com>
2024-01-14 13:21:23 +0800

    array finished

--- leptjson.c
+++ leptjson.c
@@ -34,6 +34,9 @@ typedef struct {
   size_t size, top;
 } lept_context;
 
+static int lept_parse_value(lept_context* c, lept_value* v);
+static int lept_parse_value(lept_context* c, lept_value* v);
+
 static void* lept_context_push(lept_context* c, size_t size) {
   void* ret;
   assert(size > 0);
@@ -210,6 +213,46 @@ static int lept_parse_string(lept_context* c, lept_value* v) {
   }
 }
 
+static int lept_parse_array(lept_context* c, lept_value* v) {
+  size_t size = 0;
+  int ret;
+  EXPECT(c, '[');
+  lept_parse_whitespace(c);
+  if (*c->json == ']') {
+    c->json++;
+    v->type == LEPT_ARRAY;
+    v->u.a.size = 0;
+    v->u.a.e = NULL;
+    return LEPT_PARSE_OK;
+  }
+  for (;;) {
+    lept_value e;
+    lept_init(&e);
+    if ((ret = lept_parse_value(c, &e)) != LEPT_PARSE_OK) return ret;
+    memcpy(lept_context_push(c, sizeof(lept_value)), &e, sizeof(lept_value));
+    size++;
+    lept_parse_whitespace(c);
+    if (*c->json == ',') {
+      c->json++;
+      lept_parse_whitespace(c);
+    } else if (*c->json == ']') {
+      c->json++;
+      v->type = LEPT_ARRAY;
+      v->u.a.size = size;
+      size *= sizeof(lept_value);
+      memcpy(v->u.a.e = (lept_value*)malloc(size), lept_context_pop(c, size),
+             size);
+      return LEPT_PARSE_OK;
+    } else {
+      ret = LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET;
+      break;
+    }
+    for (size_t i = 0; i < size; i++)
+      lept_free((lept_value*)lept_context_pop(c, sizeof(lept_value)));
+    return ret;
+  }
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
   switch (*c->json) {
     case 't':
@@ -220,6 +263,8 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
       return lept_parse_literal(c, v, "null", LEPT_NULL);
     case '\"':
       return lept_parse_string(c, v);
+    case '[':
+      return lept_parse_array(c, v);
     default:
       return lept_parse_number(c, v);
     case '\0':
@@ -229,7 +274,17 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
 
 void lept_free(lept_value* v) {
   assert(v != NULL);
-  if (v->type == LEPT_STRING) free(v->u.s.s);
+  switch (v->type) {
+    case LEPT_STRING:
+      free(v->u.s.s);
+      break;
+    case LEPT_ARRAY:
+      for (size_t i = 0; i < v->u.a.size; i++) lept_free(&v->u.a.e[i]);
+      free(v->u.a.e);
+      break;
+    default:
+      break;
+  }
   v->type = LEPT_NULL;
 }
 
@@ -296,4 +351,15 @@ void lept_set_string(lept_value* v, const char* s, size_t len) {
   v->u.s.s[len] = '\0';
   v->u.s.len = len;
   v->type = LEPT_STRING;
+}
+
+size_t lept_get_array_size(const lept_value* v) {
+  assert(v != NULL && v->type == LEPT_ARRAY);
+  return v->u.a.size;
+}
+
+lept_value* lept_get_array_element(const lept_value* v, size_t index) {
+  assert(v != NULL && v->type == LEPT_ARRAY);
+  assert(index < v->u.a.size);
+  return &v->u.a.e[index];
 }
--- leptjson.h
+++ leptjson.h
@@ -7,7 +7,7 @@
   do {                     \
     (v)->type = LEPT_NULL; \
   } while (0)
-
+typedef struct lept_value lept_value;
 typedef enum {
   LEPT_NULL,
   LEPT_FALSE,
@@ -18,16 +18,20 @@ typedef enum {
   LEPT_OBJECT
 } lept_type;
 
-typedef struct {
+struct lept_value {
   union {
     struct {
       char* s;
       size_t len;
     } s;
+    struct {
+      lept_value* e;
+      size_t size; /*element number*/
+    } a;
     double n;
   } u;
   lept_type type;
-} lept_value;
+};
 
 enum {
   LEPT_PARSE_OK = 0,
@@ -39,7 +43,8 @@ enum {
   LEPT_PARSE_INVALID_STRING_ESCAPE,
   LEPT_PARSE_INVALID_STRING_CHAR,
   LEPT_PARSE_INVALID_UNICODE_HEX,
-  LEPT_PARSE_INVALID_UNICODE_SURROGATE
+  LEPT_PARSE_INVALID_UNICODE_SURROGATE,
+  LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET
 };
 
 void lept_free(lept_value*);
@@ -58,4 +63,6 @@ const char* lept_get_string(const lept_value* v);
 size_t lept_get_string_length(const lept_value* v);
 void lept_set_string(lept_value* v, const char* s, size_t len);
 
+size_t lept_get_array_size(const lept_value*);
+lept_value* lept_get_array_element(const lept_value*, size_t);
 #endif /* LEPTJSON_H__ */
